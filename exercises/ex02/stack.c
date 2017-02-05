04d49ad00108a555885281638fbb4bd444f884fa
Rachel Yang <rachel.yang@students.olin.edu>
2017-02-05 18:34:50 -0500

    finished exercise 2

--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -5,6 +5,41 @@ License: GNU GPLv3
 
 */
 
+/*  Rachel Yang
+foo() sets all of the elements in the array equal to 42.
+bar() increments through the array and sets each element
+equal to its index.
+main() will print the array with the indices as its elements
+and return 0.
+
+compile error:
+stack.c: In function ‘foo’:
+stack.c:29:5: warning: function returns address of local variable [-Wreturn-local-addr]
+     return array;
+     ^
+not getting what we want -- we want what values are in the array
+but it seems like from this error the compiler is telling us
+that we are returning the address.
+
+run result:
+0
+0
+865947264
+32589
+866035040
+
+run result after uncommenting printf statements:
+0x7ffe4444c590
+0x7ffe4444c590
+0
+1
+2
+3
+4
+here the first two outputs are the addresses of foo and bar's arrays
+instead of the values within them.
+
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,7 +49,7 @@ int *foo() {
     int i;
     int array[SIZE];
 
-    //printf("%p\n", array);
+    printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
 	array[i] = 42;
@@ -26,7 +61,7 @@ void bar() {
     int i;
     int array[SIZE];
 
-    //printf("%p\n", array);
+    printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
 	array[i] = i;
